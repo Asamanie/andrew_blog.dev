ae5b053e3b007cc4d16fb22bd7d33c4c44fe0110
Andrew Samaniego <andrewsamaniego@A-S.local>
2014-06-26 16:46:13 -0500

    Blade exercise complete.

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Welcome to my Blog</title>
+</head>
+<body>
+	@yield('content')
+</body>
+</html>
--- app/views/temp/roll-dice.blade.php
+++ app/views/temp/roll-dice.blade.php
@@ -1,9 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <title>Random number between 1-6</title>
-</head>
-<body>
+@extends('layouts.master')
+
+@section('content')
 
     <h1>The random number was: {{{$random}}} </h1><br> 
     <h2>Your guess was: {{{$guess}}} <h2><br>
@@ -12,6 +9,7 @@
 		<p style="color:green;"> Your Guess was correct!</p>
 	@else
 		<p style="color:red;">Sorry, please try again!</P>
-	@endif		
-</body>
-</html>
+	@endif	
+
+@stop
+
