78baac5e59aa25548f36c5960a860ea44a52ec89
Andrew Samaniego <andrewsamaniego@A-S.local>
2014-07-07 10:28:57 -0500

    modified div class container  / is no longer fluid

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="container-fluid" style="background-color:#007791">
+<div class="container" style="background-color:#007791">
 
 <h1>{{{ $post->title}}}</h1>
 <p>{{{$post->created_at->format('F jS Y @ h:i:s A') }}}</p>
